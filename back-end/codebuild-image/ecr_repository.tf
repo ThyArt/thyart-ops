030b9826cd582ee8313f232be67ffd8774129c31
Clément Deseine <clement.deseine@gmail.com>
2019-01-09 23:57:47 +0000

    add permission to codebuild to get access to ecr image

--- back-end/codebuild-image/ecr_repository.tf
+++ back-end/codebuild-image/ecr_repository.tf
@@ -1,4 +1,4 @@
-resource "aws_ecr_repository" "codebuild_repository" {
+resource "aws_ecr_repository" "codebuild-repository" {
   name = "${var.codebuild_image_name}"
 
   tags = {
@@ -8,26 +8,26 @@ resource "aws_ecr_repository" "codebuild_repository" {
   }
 }
 
-resource "null_resource" "build_image" {
-  depends_on = ["aws_ecr_repository.codebuild_repository"]
+resource "null_resource" "build-image" {
+  depends_on = ["aws_ecr_repository.codebuild-repository"]
 
   provisioner "local-exec" {
-    command = "docker build -t ${aws_ecr_repository.codebuild_repository.repository_url} -f CodeBuild.DockerFile . "
+    command = "docker build -t ${aws_ecr_repository.codebuild-repository.repository_url} -f CodeBuild.DockerFile . "
   }
 }
 
-resource "null_resource" "login_to_ecr" {
-  depends_on = ["null_resource.build_image"]
+resource "null_resource" "login-to-ecr" {
+  depends_on = ["null_resource.build-image"]
 
   provisioner "local-exec" {
     command = "`aws ecr get-login --region ${var.aws_region} --no-include-email`"
   }
 }
 
-resource "null_resource" "push_image" {
-  depends_on = ["null_resource.login_to_ecr"]
+resource "null_resource" "push-image" {
+  depends_on = ["null_resource.login-to-ecr"]
 
   provisioner "local-exec" {
-    command = "docker push ${aws_ecr_repository.codebuild_repository.repository_url}"
+    command = "docker push ${aws_ecr_repository.codebuild-repository.repository_url}"
   }
 }
