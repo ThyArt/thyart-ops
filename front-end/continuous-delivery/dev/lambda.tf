fab7511adc622fc32d910f2fe08780ba849bd846
Alexandre Lambrinidis <alambrinidis@gmail.com>
2019-01-25 17:26:01 +0000

    CodePipeline is implemented with terraform, upload to S3 bucket is functional

--- front-end/continuous-delivery/dev/lambda.tf
+++ front-end/continuous-delivery/dev/lambda.tf
@@ -4,6 +4,7 @@ resource "aws_lambda_function" "statics3deploy" {
   filename         = "../../deployment-lambda/statics3deploy.zip"
   source_code_hash = "${base64sha256(file("../../deployment-lambda/statics3deploy.zip"))}"
   runtime          = "nodejs8.10"
+  timeout          = "10"
   role             = "${aws_iam_role.dev-lambda-role.arn}"
 }
 
