b60b625d8399b30cad557d64ece90aa1d840d259
Clément Deseine <clement.deseine@gmail.com>
2019-04-26 22:00:26 +0100

    remove unused data

--- back-end/environment/dev/data.tf
+++ back-end/environment/dev/data.tf
@@ -2,11 +2,3 @@ data "aws_elastic_beanstalk_solution_stack" "dev-solution-stack" {
   name_regex  = "^64bit Amazon Linux 2018.03 (.*) running PHP 7.2$"
   most_recent = true
 }
-
-data "aws_s3_bucket" "codebuild-bucket" {
-  bucket = "${var.application_name}-codebuild"
-}
-
-data "aws_s3_bucket" "codepipeline-bucket" {
-  bucket = "${var.application_name}-codebuild"
-}
